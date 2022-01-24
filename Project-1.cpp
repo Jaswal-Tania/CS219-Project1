c2325dddac9e624d477dcbbf17de22bc470ac8ba
Tania Jaswal <tjaswal@nevada.unr.edu>
2022-01-23 22:22:03 -0800

    1

--- Project-1.cpp
+++ Project-1.cpp
@@ -3,15 +3,14 @@
 using namespace std;
 
 int main(){
-   fstream myfile;
    //myfile.open("Programming-Project-1.txt", ios::in);
-   fstream myfile("Programming-Project-1.txt");
+   ifstream myfile("Programming-Project-1.txt");
    string operation;
    unsigned int operand1;
    unsigned int operand2;
 
    while(myfile >> operation >> operand1 >> operand2){
-       operation = '+';
-       cout << operand1 << operation << operand2 << endl;
+       //operation = '+';
+       cout << operand1 << '+' << operand2 << endl;
    }
 }
