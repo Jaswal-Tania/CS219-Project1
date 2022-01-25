a156ead7b4101b568457fab46b734218532d5dc4
Tania Jaswal <tjaswal@nevada.unr.edu>
2022-01-25 15:29:41 -0800

    1

--- Project-1.cpp
+++ Project-1.cpp
@@ -1,16 +1,29 @@
 #include<iostream>
+#include <string>
+#include <sstream>
 #include<fstream>
+#include<iomanip>
+
+
 using namespace std;
 
 int main(){
-   //myfile.open("Programming-Project-1.txt", ios::in);
-   ifstream myfile("Programming-Project-1.txt");
-   string operation;
-   unsigned int operand1;
-   unsigned int operand2;
+    std::fstream myfile;
+    myfile.open("Programming-Project-1.txt", std::ios::in);
+    //std::ifstream myfile("Programming-Project-1.txt");
+    int value;
+    std::string operation;
+    uint32_t operand1;
+    uint32_t operand2;
+    stringstream ss;
+   
 
-   while(myfile >> operation >> operand1 >> operand2){
-       //operation = '+';
-       cout << operand1 << '+' << operand2 << endl;
+    if (!myfile){  // print message if file is not open
+            std::cout << "File can not be open\n";
+        }
+
+    myfile >> std::hex;
+    while(myfile >> operation >> operand1 >> operand2){
+         ss >> hex>> operand1 >>;
    }
 }
